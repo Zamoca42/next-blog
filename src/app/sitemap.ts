252b5a59517bc7a43b6b191e84429f54cae2738d
Chooooooo <suntail2002@naver.com>
2024-06-12 12:53:17 +0900

    Fix: 사이트맵 형식 수정
    - 잘못된 형식 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,12 +2,23 @@ import { MetadataRoute } from "next";
 import { getAllPosts } from "@/app/api/action";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const host = "https://www.zamoca.space";
   const posts = await getAllPosts();
 
-  const postUrls = posts.map((post) => ({
-    url: `https://www.zamoca.space/posts${post.slug}`,
+  const postUrls: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `${host}/posts${post.slug}`,
     lastModified: post.updatedAt,
+    changeFrequency: "weekly",
+    priority: 0.8,
   }));
 
-  return [{ url: "/", lastModified: new Date() }, ...postUrls];
+  return [
+    {
+      url: host,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    ...postUrls,
+  ];
 }
