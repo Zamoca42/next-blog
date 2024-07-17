1a616ac18b41aeac2f36a9ff6247173971b43d2b
Chooooooo <suntail2002@naver.com>
2024-07-17 20:59:35 +0900

    Chore: 이미지 설정 변경
    - vw에서 width 설정으로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Container from "@/component/ui/container";
 import { HeroPost } from "@/component/home/hero-post";
 import { Intro } from "@/component/home/intro";
-import { MoreStories } from "@/component/home/more-stories";
+// import { MoreStories } from "@/component/home/more-stories";
 import Footer from "@/component/home/footer";
 import { getAllPosts } from "@/app/api/action";
+
+import dynamic from "next/dynamic";
+
+const MoreStories = dynamic(() =>
+  import("@/component/home/more-stories").then((mod) => mod.MoreStories)
+);
 
 export default async function Index() {
   const [heroPost, ...previews] = await getAllPosts();
