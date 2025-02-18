2ab82b8bfbcc226a99cd5f8bebabfb09c2099d49
Chooooooo <contact@choo.ooo>
2025-02-18 15:17:31 +0900

    Chore: Next.js 15 업그레이드

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,24 @@
 // @ts-check
+import type { NextConfig } from 'next';
 import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants.js";
 import { savePostMetadata, shouldUpdateGitInfo } from "./src/script/post-index.js";
 import { updateAlgoliaIndex } from "./src/script/algolia-index.js";
-import { generateRssFeed } from "./src/script/generate-rss.js";
 import bundleAnalyzer from '@next/bundle-analyzer';
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
-/**
- * @typedef {import('next').NextConfig} NextConfig
- */
-
-/**
- * @param {string} phase
- * @returns {Promise<NextConfig>}
- */
-export default async (phase) => {
+export default async (phase: string): Promise<NextConfig> => {
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-      await savePostMetadata();
-      await generateRssFeed();
+    await savePostMetadata();
   }
 
   if (phase === PHASE_PRODUCTION_BUILD && process.env.VERCEL_ENV === "production") {
     await updateAlgoliaIndex();
   }
 
-  /** @type {NextConfig} */
-  const nextConfig = {
+  const nextConfig: NextConfig = {
     images: {
       imageSizes: [32, 48, 64, 96],
       deviceSizes: [750, 828, 1080, 1200],
@@ -36,22 +26,32 @@ export default async (phase) => {
         {
           protocol: 'https',
           hostname: 'github.com',
+          port: '',
+          search: '',
         },
         {
           protocol: 'https',
           hostname: "user-images.githubusercontent.com",
+          port: '',
+          search: '',
         },
         {
           protocol: 'https',
           hostname: "capsule-render.vercel.app",
+          port: '',
+          search: '',
         },
         {
           protocol: 'https',
           hostname: "img.shields.io",
+          port: '',
+          search: '',
         },
         {
           protocol: 'https',
           hostname: "github-readme-stats.vercel.app",
+          port: '',
+          search: '',
         },
       ],
     },
